d8b8b3abe94a27f276fe8f1cc01849585f401225
ishikawa_atsuko <ishikawa.atsuko@m.alhinc.jp>
2020-06-02 00:49:50 +0900

    java8を使って修正

--- src/main/java/com/tool/linebot/EchoApplication.java
+++ src/main/java/com/tool/linebot/EchoApplication.java
@@ -16,6 +16,15 @@
 
 package com.tool.linebot;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.StringJoiner;
+import java.util.stream.Collectors;
+
+import javax.script.Bindings;
+import javax.script.ScriptEngineManager;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -32,21 +41,50 @@ import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 @SpringBootApplication
 @LineMessageHandler
 public class EchoApplication {
-    private final Logger log = LoggerFactory.getLogger(EchoApplication.class);
+	private final Logger log = LoggerFactory.getLogger(EchoApplication.class);
 
-    public static void main(String[] args) {
-        SpringApplication.run(EchoApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(EchoApplication.class, args);
+	}
 
-    @EventMapping
-    public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
-        log.info("event: " + event);
-        final String originalMessageText = event.getMessage().getText();
-        return new TextMessage(originalMessageText);
-    }
+	@EventMapping
+	public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+		String message = "失敗";
+		if ("福岡の天気".equals(event)){
+			try {
+				URL url = new URL("http://weather.livedoor.com/forecast/webservice/json/v1?city=130010");
+				String json;
+				// Get通信してStringに（evalするために丸括弧で囲む）
+				try (BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));) {
+					json = br.lines().collect(Collectors.joining("", "(", ")"));
+				}// ↑ "({"forecasts":[{"dateLabel":"x",...},{"dateLabel":"y",...},...],...})" こんなString
 
-    @EventMapping
-    public void handleDefaultMessageEvent(Event event) {
-        System.out.println("event: " + event);
-    }
+				Bindings jsObj = (Bindings) new ScriptEngineManager().getEngineByName("js").eval(json);
+
+				jsObj = (Bindings) jsObj.get("forecasts");
+				// get(key)で取得できるが、戻りはObject型。必要に応じてキャスト
+
+				StringJoiner sj = new StringJoiner("\t");
+
+				// js配列は values() で
+				jsObj.values().stream()
+				.map(o -> (Bindings) o)
+				.map(o -> o.get("dateLabel") + "\t" + o.get("telop"))
+				.forEach(i -> sj.add(String.valueOf(i)));
+				message = sj.toString();
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}// 例外処理はまるっと雑にしちゃっています。ごめんなさい。
+		} else {
+		log.info("event: " + event);
+		message = event.getMessage().getText();
+		}
+		return new TextMessage(message);
+	}
+
+	@EventMapping
+	public void handleDefaultMessageEvent(Event event) {
+		System.out.println("event: " + event);
+	}
 }
